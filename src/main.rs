539ba1eed34905c85f56edb527dd30244f3345ec
panicbit <panicbit.dev@gmail.com>
2018-06-28 15:27:01 +0200

    Init

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+extern crate psutil;
+
+use std::net::UdpSocket;
+use std::net::IpAddr;
+use std::process::Command;
+
+const START_PHRASE: &[u8] = b"YatseStart";
+
+fn main() {
+    let ip = IpAddr::from([0, 0, 0, 0]);
+    let port = 5600;
+    let addr = (ip, port);
+    let socket = UdpSocket::bind(addr).unwrap();
+
+    loop {
+        let mut data = [0; 4 * 1024];
+        socket.recv_from(&mut data).ok();
+
+        for snippet in data.windows(START_PHRASE.len()) {
+            if snippet == START_PHRASE {
+                start_kodi();
+            }
+        }
+    }
+}
+
+fn start_kodi() {
+    if process_list().iter().find(|ps| ps.comm == "kodi").is_some() {
+        println!("Kodi is already running");
+        return;
+    }
+
+    if let Err(e) = Command::new("kodi").spawn() {
+        println!("{}", e);
+    }
+}
+
+fn process_list() -> Vec<psutil::process::Process> {
+    loop {
+        match psutil::process::all() {
+            Ok(procs) => return procs,
+            Err(e) => {
+                if e.kind() != std::io::ErrorKind::NotFound {
+                    println!("{}", e);
+                    return Vec::new();
+                }
+            }
+        }
+    }
+}
